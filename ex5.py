9526be74736ae27b865a032176d82c86651eea33
stenyJ <79965959+stenyJ@users.noreply.github.com>
2023-10-11 16:41:01 +0530

    demo py

--- ex5.py
+++ ex5.py
@@ -1,8 +1,63 @@
-name=input("Hello, what is your name?: ")
-print(name,"!\n")
-old=int(input("how old are you?"))
-after=old+5
-before=old-5
-print("Did you know that in five years you will be",after,"years old?")
-print("After five years ago you were",before,"!Imagine that!")
+# name=input("Hello, what is your name?: ")
+# print(name,"!\n")
+# old=int(input("how old are you?"))
+# after=old+5
+# before=old-5
+# print("Did you know that in five years you will be",after,"years old?")
+# print("After five years ago you were",before,"!Imagine that!")
+
+# def sum() :
+#        a = 10
+#        b = 5
+#        print( a + b ),
+#     #    print("before")
+#     #    print("after")
+# sum()
+
+# def sum(a,b):
+#     c=a+b
+#     return c
+# result1=sum(5,10)
+# result2=sum(100,50)
+# result3=sum(1000,500)
+# print(result1)
+# print(result2)
+# print(result3)
+
+# def find(a,b) :
+#     if a%2==0:
+#         print (a,'is Even')
+#     if b%2==0:
+#         print (b,'is Even')   
+    
+#     else:
+#         print("Odd")
+    
+# find(5,6)
+
+# def is_voting_age():
+#     age = int(input("Enter your age: "))
+#     if age >= 18:
+#         return 'Eligible'
+#     else:
+#         return 'Not eligible'
+
+# result = is_voting_age()
+# print(result)
+
+
+# def is_leap_year(year):
+#     if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+#         return True
+#     else:
+#         return False
+
+li=[1,2,3,4,5]
+for num in li :
+    if num %2==0:
+        print(num)
+
+
+
+    
 
